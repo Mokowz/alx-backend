2407b8a2fc154fb3963b304733d63f23c7ce3613
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-20 11:48:42 +0300

    Implement the LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""LRU Cache class"""
+from base_caching import BaseCaching
+from typing import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """LRU Cache"""
+    def __init__(self):
+        """Init"""
+        super().__init__()
+        self.lru_list = OrderedDict()
+
+    def put(self, key, item):
+        """Add Values"""
+        if key and item:
+            self.lru_list[key] = item
+            self.lru_list.move_to_end(key)
+            self.cache_data[key] = item
+
+            if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+                remove = next(iter(self.lru_list))
+                del self.cache_data[remove]
+                print(f"DISCARD: {remove}")
+
+            if len(self.lru_list) > BaseCaching.MAX_ITEMS:
+                self.lru_list.popitem(last=False)
+
+    def get(self, key):
+        """Get values"""
+        if key in self.cache_data:
+            self.lru_list.move_to_end(key)
+            return self.cache_data.get(key)
+        return None
