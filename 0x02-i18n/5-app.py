9e194526b0fefb763285a1226b3170a816e58f02
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-27 13:33:22 +0300

    User logging implemented

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,1 +1,60 @@
 #!/usr/bin/env python3
+"""Get locale module"""
+import babel
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """config class for babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Get language code from config"""
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(login_as):
+    """Return a user id"""
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return
+
+
+@app.before_request
+def before_request():
+    """Get done before everything else"""
+    g.user = get_user(request.args.get("login_as"))
+
+
+@app.route('/')
+def home():
+    """Home function"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
