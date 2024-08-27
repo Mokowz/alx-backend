dff86e54fba2026ed9d06ea1e35e59e9349ed02c
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-27 12:03:16 +0300

    Implement translations

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""Instantiate babel module"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config class for babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    """Default route function"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,73 @@
 #!/usr/bin/env python3
+"""Get locale module"""
+# import babel
+# from flask import Flask, render_template ,request
+# from flask_babel import Babel
+
+
+# app = Flask(__name__)
+# babel = Babel(app)
+
+
+# class Config:
+#     """config class for babel"""
+#     LANGUAGES = ['en', 'fr']
+#     BABEL_DEFAULT_LOCALE = 'en'
+#     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+# app.config.from_object(Config)
+
+# @babel.localeselector
+# def get_locale():
+#     """Get language code from config"""
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# @app.route('/')
+# def home():
+#    """Home function"""
+#    return render_template('2-index.html')
+
+
+# if __name__ == '__main__':
+#    app.run(debug=True)
+
+
+import babel
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+     determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
