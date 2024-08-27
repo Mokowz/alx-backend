63fc6a7e37659c53a0ee09ba04ec79dc87995398
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-27 13:47:45 +0300

    Implement the get timezone function

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,1 +1,81 @@
 #!/usr/bin/env python3
+"""Get locale module"""
+import babel
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """config class for babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Get language code from config"""
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """ get_timezone function"""
+    try:
+        timezone = request.args.get("timezone")
+        if timezone:
+            return pytz.timezone(timezone)
+        user = request.args.get("login_as")
+        if user:
+            timezone = users.get(int(user)).get('timezone')
+            if timezone:
+                return pytz.timezone(timezone)
+        timezone = request.headers.get("timezone")
+        if timezone:
+            return pytz.timezone(timezone)
+    except pytz.UnknownTimeZoneError:
+        return app.config.get('BABEL_DEFAULT_TIMEZONE')
+    return app.config.get('BABEL_DEFAULT_TIMEZONE')
+
+
+def get_user(login_as):
+    """Return a user id"""
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return
+
+
+@app.before_request
+def before_request():
+    """Get done before everything else"""
+    g.user = get_user(request.args.get("login_as"))
+
+
+@app.route('/')
+def home():
+    """Home function"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
